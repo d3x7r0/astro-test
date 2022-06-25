ec32979f944cf3937c22a5031645cb420619e74d
Luis Nabais <dextro@gmail.com>
2022-06-25 16:44:15 +0100

    Updated dependencies and config

--- main/astro.config.mjs
+++ main/astro.config.mjs
@@ -5,4 +5,9 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
+  vite: {
+    ssr: {
+      noExternal: ['components']
+    }
+  }
 });
